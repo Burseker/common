3675f0760b730110f950c44937e92da2822988b4
burseker <spybreakp-2@yandex.ru>
2022-12-20 01:02:56 +0300

    set_env scripts

---
+++ scripts/set_env/java8_env.cmd
@@ -0,0 +1,11 @@
+﻿@echo off
+@REM Устанавливаем путь для JDK
+set "PATH=C:\WrkPrFiles\JDK\java-se-8u41-ri\bin;%PATH%"
+@REM Необязательно но тоже можно
+set "JAVA_HOME=C:\WrkPrFiles\JDK\java-se-8u41-ri"
+
+@REM Устанавливаем путь для MAVEN
+set "PATH=c:\Users\burakov\.m2\wrapper\dists\apache-maven-3.5.4-bin\jl1qqhdeineh9qg83dtj7i91c\apache-maven-3.5.4\"
+
+@REM Устанавливаем кодировку файлов, используемых и поставляемых Java инструментарием в UTF-8
+set "JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8"
